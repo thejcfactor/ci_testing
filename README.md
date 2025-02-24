497bbda7c200722fb2c93f89a05a8cb077693c33
Jared Casey <jared.casey@couchbase.com>
2025-02-24 12:37:12 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+**NOTE:** This is merely a temporary repository that will be deleted once the migration is complete.
---
+++ ci/gha.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+set -e -u
+
+function display_info {
+    echo "Workflow Run ID=$GITHUB_RUN_ID"
+    echo "is_release=$INPUT_IS_RELEASE"
+    echo "SHA=$$INPUT_SHA"
+    echo "version=$INPUT_VERSION"
+    echo "cxx_change=$INPUT_CXX_CHANGE"
+    echo "DEFAULT_PYTHON=$DEFAULT_PYTHON"
+    echo "PYTHON_VERSIONS=$PYTHON_VERSIONS"
+    echo "X86_64_PLATFORMS=$X86_64_PLATFORMS"
+    echo "ARM64_PLATFORMS=$ARM64_PLATFORMS"
+}
+
+function validate_input {
+    echo "validate_input"
+}
+
+cmd=$1
+
+if [ "$cmd" == "display_info" ]; then
+    display_info
+elif [ "$cmd" == "validate_input" ]; then
+    validate_input
+else
+    echo "Invalid command"
+fi
